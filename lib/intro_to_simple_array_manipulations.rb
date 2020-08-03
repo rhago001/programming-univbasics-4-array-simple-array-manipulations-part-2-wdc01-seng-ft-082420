8127f024648247ecc867fc8812d32fb32609b93e
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-03 09:09:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,8 +17,8 @@ def using_flatten(instruments)
   flat_array = instruments.flatten
 end
 
-def using_delete
-  no_offense_steven = using_delete(instructors, "Steven")
+def using_delete(instructors
+  no_offense_steven = using_delete, "Steven")
   
 end
 
