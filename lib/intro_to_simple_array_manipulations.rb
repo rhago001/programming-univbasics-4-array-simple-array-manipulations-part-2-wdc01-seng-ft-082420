324151c4558c9fa25973d7d09bf76343293eae07
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-03 08:57:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@
   @all_my_favs =  my_favorite_things. concat (more_favs)
 end 
 
-def using_insert
-  @new_array = using_insert(@list_of_programming_languages, @another_language)
+def using_insert (list_of_programming_languages, another_language)
+  @new_array = (list_of_programming_languages, another_language)
   
 end 
