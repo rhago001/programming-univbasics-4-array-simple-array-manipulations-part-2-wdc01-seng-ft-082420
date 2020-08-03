c86dc56dd48ee7a52c24a19eee835a91c1b5b9c5
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-03 08:59:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,5 +4,11 @@ end
 
 def using_insert (list_of_programming_languages, another_language)
   @new_array = (list_of_programming_languages).insert(4, another_language)
+end 
+
+
+def using_uniq
+  
+  
   
 end 
